1ddceb6caa7e3433ac6725a43bef5c520e37cf62
Gaurav Mehla <gaurav@mehla.in>
2023-07-19 12:51:37 -0700

    admin section and ads section

--- src/main/java/com/example/playquest/controllers/Admin.java
+++ src/main/java/com/example/playquest/controllers/Admin.java
@@ -1,32 +1,39 @@
 package com.example.playquest.controllers;
 
-import com.example.playquest.entities.UserSession;
 import com.example.playquest.services.SessionManager;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Optional;
-
 @Controller
 @AllArgsConstructor
-public class Home {
+public class Admin {
 
     private final SessionManager sessionManager;
 
-    @GetMapping("/")
-    public String Index(HttpServletRequest request) {
+    @GetMapping("/admin")
+    public String Administrator(HttpServletRequest request) {
         // Check if the user is logged in or has an active session
         if (!sessionManager.isUserLoggedIn(request)) {
             return "redirect:/login";
         }
 
         // If the user is logged in, proceed with the home page logic
-        return "index";
+        return "admin/index";
     }
+
+    @GetMapping("/admin/ads")
+    public String Ads(HttpServletRequest request) {
+        // Check if the user is logged in or has an active session
+        if (!sessionManager.isUserLoggedIn(request)) {
+            return "redirect:/login";
+        }
+
+        // If the user is logged in, proceed with the home page logic
+        return "admin/ads";
+    }
+
 
 }
